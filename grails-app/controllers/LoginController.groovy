72ed00dffb25b6e67e509d961e6107eb5c35f242
erajasekar <e.rajasekar@gmail.com>
2011-03-13 21:56:32 -0400

    Redirect to previously accessed url after successful login

--- grails-app/controllers/LoginController.groovy
+++ grails-app/controllers/LoginController.groovy
@@ -11,107 +11,119 @@ import org.springframework.security.authentication.LockedException
 import org.springframework.security.core.context.SecurityContextHolder as SCH
 import org.springframework.security.web.WebAttributes
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.savedrequest.SavedRequest
+
+import javax.servlet.http.HttpSession
 
 class LoginController {
 
-	/**
-	 * Dependency injection for the authenticationTrustResolver.
-	 */
-	def authenticationTrustResolver
+  /**
+   * Dependency injection for the authenticationTrustResolver.
+   */
+  def authenticationTrustResolver
 
-	/**
-	 * Dependency injection for the springSecurityService.
-	 */
-	def springSecurityService
+  /**
+   * Dependency injection for the springSecurityService.
+   */
+  def springSecurityService
 
-	/**
-	 * Default action; redirects to 'defaultTargetUrl' if logged in, /login/auth otherwise.
-	 */
-	def index = {
-		if (springSecurityService.isLoggedIn()) {
-			redirect uri: params.targetUrl;
-		}
-		else {
-			redirect action: auth, params: params
-		}
-	}
+  /**
+   * Default action; redirects to 'defaultTargetUrl' if logged in, /login/auth otherwise.
+   */
+  def index = {
+    if (springSecurityService.isLoggedIn()) {
+      redirect uri: params.targetUrl;
+    }
+    else {
+      redirect action: auth, params: params
+    }
+  }
 
-    /**
-	 * Show the login page.
-	 */
-	def auth = {
+  /**
+   * Show the login page.
+   */
+  def auth = {
 
-        def config = SpringSecurityUtils.securityConfig
+    def config = SpringSecurityUtils.securityConfig
 
-		String view = 'auth'
-		String postUrl = "${request.contextPath}${config.apf.filterProcessesUrl}"
-        render view: view, model: [postUrl: postUrl,
-		                           rememberMeParameter: config.rememberMe.parameter]
-	}
+    String view = 'auth'
+    String postUrl = "${request.contextPath}${config.apf.filterProcessesUrl}"
+    HttpSession session = request.getSession(false);
+    //Redirect to previously accessed url, if user tries to access a page without logging in and user is redirected to login page.
+    if (session != null) {
+      SavedRequest savedRequest = (SavedRequest) session.getAttribute(WebAttributes.SAVED_REQUEST);
+      if (savedRequest != null) {
+        params.targetUrl = savedRequest.getRedirectUrl() - "$request.scheme://$request.serverName:$request.serverPort$request.contextPath";
 
-
-    def authSuccess = {
-      session.user =  springSecurityService.currentUser;
-      redirect uri:params.targetUrl;
+      }
     }
 
+    render view: view, model: [postUrl: postUrl,
+            rememberMeParameter: config.rememberMe.parameter]
+  }
 
-	/**
-	 * Show denied page.
-	 */
-	def denied = {
-		if (springSecurityService.isLoggedIn() &&
-				authenticationTrustResolver.isRememberMe(SCH.context?.authentication)) {
-			// have cookie but the page is guarded with IS_AUTHENTICATED_FULLY
-			redirect action: full, params: params
-		}
-	}
 
-	/**
-	 * Login page for users with a remember-me cookie but accessing a IS_AUTHENTICATED_FULLY page.
-	 */
-	def full = {
-		def config = SpringSecurityUtils.securityConfig
-		render view: 'auth', params: params,
-			model: [hasCookie: authenticationTrustResolver.isRememberMe(SCH.context?.authentication),
-			        postUrl: "${request.contextPath}${config.apf.filterProcessesUrl}"]
-	}
+  def authSuccess = {
+    session.user = springSecurityService.currentUser;
+    redirect uri: params.targetUrl;
+  }
 
-	/**
-	 * Callback after a failed login. Redirects to the auth page with a warning message.
-	 */
-	def authfail = {
+  /**
+   * Show denied page.
+   */
+  def denied = {
+    if (springSecurityService.isLoggedIn() &&
+            authenticationTrustResolver.isRememberMe(SCH.context?.authentication)) {
+      // have cookie but the page is guarded with IS_AUTHENTICATED_FULLY
+      redirect action: full, params: params
+    }
+  }
 
-        def refererUrl = request.getHeader("Referer");
-        def username = session[UsernamePasswordAuthenticationFilter.SPRING_SECURITY_LAST_USERNAME_KEY]
-		String msg = ''
-		def exception = session[WebAttributes.AUTHENTICATION_EXCEPTION]
+  /**
+   * Login page for users with a remember-me cookie but accessing a IS_AUTHENTICATED_FULLY page.
+   */
+  def full = {
+    def config = SpringSecurityUtils.securityConfig
+    render view: 'auth', params: params,
+            model: [hasCookie: authenticationTrustResolver.isRememberMe(SCH.context?.authentication),
+                    postUrl: "${request.contextPath}${config.apf.filterProcessesUrl}"]
+  }
 
-		if (exception) {
-			if (exception instanceof AccountExpiredException) {
-				msg = SpringSecurityUtils.securityConfig.errors.login.expired
-			}
-			else if (exception instanceof CredentialsExpiredException) {
-				msg = SpringSecurityUtils.securityConfig.errors.login.passwordExpired
-			}
-			else if (exception instanceof DisabledException) {
-				msg = SpringSecurityUtils.securityConfig.errors.login.disabled
-			}
-			else if (exception instanceof LockedException) {
-				msg = SpringSecurityUtils.securityConfig.errors.login.locked
-			}
-			else {
-				msg = "spring.security.ui.login.fail";
-			}
-		}
-        if (springSecurityService.isAjax(request)) {
-			render([error: msg] as JSON)
-		}
-		else {
-			flash.message = msg
-			redirect url:refererUrl;
-		}
-	}
+  /**
+   * Callback after a failed login. Redirects to the auth page with a warning message.
+   */
+  def authfail = {
 
-	
+    def refererUrl = request.getHeader("Referer");
+    def username = session[UsernamePasswordAuthenticationFilter.SPRING_SECURITY_LAST_USERNAME_KEY]
+    String msg = ''
+    def exception = session[WebAttributes.AUTHENTICATION_EXCEPTION]
+
+    if (exception) {
+      if (exception instanceof AccountExpiredException) {
+        msg = SpringSecurityUtils.securityConfig.errors.login.expired
+      }
+      else if (exception instanceof CredentialsExpiredException) {
+        msg = SpringSecurityUtils.securityConfig.errors.login.passwordExpired
+      }
+      else if (exception instanceof DisabledException) {
+          msg = SpringSecurityUtils.securityConfig.errors.login.disabled
+        }
+        else if (exception instanceof LockedException) {
+            msg = SpringSecurityUtils.securityConfig.errors.login.locked
+          }
+          else {
+            msg = "spring.security.ui.login.fail";
+          }
+    }
+    if (springSecurityService.isAjax(request)) {
+      render([error: msg] as JSON)
+    }
+    else {
+      flash.message = msg
+      redirect url: refererUrl;
+    }
+  }
+
+
 }
