6c438968aa59aa7fb582cb3b9d3c0308dbcbb868
e.rajasekar <e.rajasekar@makkaldeals.com>
2011-03-29 18:07:48 -0400

    dont create test data in prod

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,6 +3,7 @@ import com.grepdeals.auth.*;
 import org.codehaus.groovy.grails.commons.ConfigurationHolder as CH
 import com.grepdeals.Post
 import com.grepdeals.Business;
+import grails.util.Environment;
 
 class BootStrap {
 
@@ -20,9 +21,9 @@ class BootStrap {
 
     def roleAdmin = Role.findByAuthority(Role.ROLE_ADMIN) ?: new Role(authority: Role.ROLE_ADMIN).save();
 
-    Role.findByAuthority(Role.ROLE_CLIENT) ?: new Role(authority: Role.ROLE_CLIENT).save();
+      Role.findByAuthority(Role.ROLE_CLIENT) ?: new Role(authority: Role.ROLE_CLIENT).save();
 
-    Role.findByAuthority(Role.ROLE_CUSTOMER) ?: new Role(authority: Role.ROLE_CUSTOMER).save();
+      Role.findByAuthority(Role.ROLE_CUSTOMER) ?: new Role(authority: Role.ROLE_CUSTOMER).save();
 
     def admin = new User(email: CH.config.grepdeals.user.admin.email, password: password, enabled: true).save();
 
@@ -30,38 +31,44 @@ class BootStrap {
   }
 
   def createTestData() {
-    def clientRole = Role.findByAuthority(Role.ROLE_CLIENT)
-    def customerRole = Role.findByAuthority(Role.ROLE_CUSTOMER)
-    String password = springSecurityService.encodePassword("grepdeals");
 
-    def u1 = User.build(email:'client1@grepdeals.com' , password:password , enabled:true, accountLocked : false, areaCode : "33634");
-    UserRole.create(u1,clientRole,true);
+    // Lets not create test data in production
 
-    def b1 = Business.build();
-    def b2 = Business.build(name:b1.name,category:b1.category, city:'Miami' , areaCode:"45678");// same name as b1, but different city,areacode
-    def b3 = Business.build(name:b1.name,category:b1.category, areaCode:"33635");
-    Map customers = [:];
+    if (!Environment.current.equals(Environment.PRODUCTION)) {
+      
+      def clientRole = Role.findByAuthority(Role.ROLE_CLIENT)
+      def customerRole = Role.findByAuthority(Role.ROLE_CUSTOMER)
+      String password = springSecurityService.encodePassword("grepdeals");
 
-    customers.c0 = Customer.build(email:"e.rajasekar@grepdeals.com" , business:b1, password:password);
-    customers.c1 = Customer.build(business:b1, password:password);
-    customers.c2 = Customer.build(business:b1, password:password);
-    customers.c3 = Customer.build(business:b2, password:password);
-    customers.c4 = Customer.build(business:b3, password:password);
-    def p11 = Post.build(author:customers.c1);
-    def p12 = Post.build(author:customers.c1);
-    def p21 = Post.build(author:customers.c2);
-    def p31 = Post.build(author:customers.c3);
-    def p41 = Post.build(author:customers.c4);
-    p11.parseTags("a");
-    p12.parseTags("a");
-    p21.parseTags("b");
-    p31.parseTags("b");
-    p41.parseTags("c")
-    customers.values().each {  c->
-      UserRole.create(c, customerRole,true);
+      def u1 = User.build(email: 'client1@grepdeals.com', password: password, enabled: true, accountLocked: false, areaCode: "33634");
+      UserRole.create(u1, clientRole, true);
+
+      def b1 = Business.build();
+      def b2 = Business.build(name: b1.name, category: b1.category, city: 'Miami', areaCode: "45678");// same name as b1, but different city,areacode
+      def b3 = Business.build(name: b1.name, category: b1.category, areaCode: "33635");
+      Map customers = [:];
+
+      customers.c0 = Customer.build(email: "e.rajasekar@grepdeals.com", business: b1, password: password);
+      customers.c1 = Customer.build(business: b1, password: password);
+      customers.c2 = Customer.build(business: b1, password: password);
+      customers.c3 = Customer.build(business: b2, password: password);
+      customers.c4 = Customer.build(business: b3, password: password);
+      def p11 = Post.build(author: customers.c1);
+      def p12 = Post.build(author: customers.c1);
+      def p21 = Post.build(author: customers.c2);
+      def p31 = Post.build(author: customers.c3);
+      def p41 = Post.build(author: customers.c4);
+      p11.parseTags("a");
+      p12.parseTags("a");
+      p21.parseTags("b");
+      p31.parseTags("b");
+      p41.parseTags("c")
+      customers.values().each {  c ->
+        UserRole.create(c, customerRole, true);
+      }
     }
 
-   
+
   }
 
   def destroy = {
