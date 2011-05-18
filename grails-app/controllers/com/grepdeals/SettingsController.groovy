2d673a05a83642fd90946bb4f4fa13d192b506e0
erajasekar <e.rajasekar@gmail.com>
2011-05-17 21:34:33 -0400

    fix settings redirect

--- grails-app/controllers/com/grepdeals/SettingsController.groovy
+++ grails-app/controllers/com/grepdeals/SettingsController.groovy
@@ -1,9 +1,18 @@
-import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+package com.grepdeals
+
+import grails.plugins.springsecurity.Secured;
 
 class SettingsController {
 
     static navigation = true;
 
-	def index = { render view: '/settings/settings' }
-	
+	def index = {
+
+      redirect action:'showSettings';
+    }
+
+    @Secured(['IS_AUTHENTICATED_REMEMBERED'])
+    def showSettings = {
+      render view:'settings';
+    }
 }
