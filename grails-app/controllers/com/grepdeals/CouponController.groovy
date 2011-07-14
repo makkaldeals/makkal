061252704ff745eb115365d37679d420e599bda9
krish777 <krish777@gmail.com>
2011-07-13 19:36:56 -0500

    removed println

--- grails-app/controllers/com/grepdeals/CouponController.groovy
+++ grails-app/controllers/com/grepdeals/CouponController.groovy
@@ -14,17 +14,13 @@ class CouponController {
 			  try{
 				byte[] b
 				def baseUri = request.scheme + "://" + request.serverName + ":" + request.serverPort + grailsAttributes.getApplicationUri(request)
-				// def baseUri = g.createLink(uri:"/", absolute:"true").toString()
 				// TODO: get this working...
 				if(params.template){
-				  println "*******************Template: $params.template"
 				  def content = g.render(template:params.template, model:[pdf:params])
 				  b = pdfService.buildPdfFromString(content.readAsString(), baseUri)
 				}
 				if(params.pdfController){
 					
-					println "Controller --------------- "
-				  //println "GSP - Controller: $params.pdfController , Action: $params.pdfAction, Id: $params.pdfId"
 				  def content = g.include(controller:params.pdfController, action:params.pdfAction, id:params.pdfId)
 				  b = pdfService.buildPdfFromString(content.readAsString(), baseUri)
 				}
@@ -38,46 +34,12 @@ class CouponController {
 				response.getOutputStream().write(b)
 			  }
 			  catch (Throwable e) {
-				println "there was a problem with PDF generation ${e}"
-				//if(params.template) render(template:params.template)
 				if(params.pdfController) redirect(controller:params.pdfController, action:params.pdfAction, params:params)
 				else redirect(uri:params.url + '?' + request.getQueryString())
 			  }
 			}
 		  
-	/*	  def pdfLink = {
-			try{
-			  byte[] b
-			  def baseUri = request.scheme + "://" + request.serverName + ":" + request.serverPort + grailsAttributes.getApplicationUri(request)
-			  // def baseUri = g.createLink(uri:"/", absolute:"true").toString()
-			  // TODO: get this working...
-			  //if(params.template){
-				//println "Template: $params.template"
-				//def content = g.render(template:params.template, model:[pdf:params])
-				//b = pdfService.buildPdfFromString(content.readAsString(), baseUri)
-			  //}
-			  if(params.pdfController){
-				//println "GSP - Controller: $params.pdfController , Action: $params.pdfAction, Id: $params.pdfId"
-				def content = g.include(controller:params.pdfController, action:params.pdfAction, id:params.pdfId)
-				b = pdfService.buildPdfFromString(content.readAsString(), baseUri)
-			  }
-			  else{
-				def url = baseUri + params.url
-				b = pdfService.buildPdf(url)
-			  }
-			  response.setContentType("application/pdf")
-			  response.setHeader("Content-disposition", "attachment; filename=" + (params.filename ?: "document.pdf"))
-			  response.setContentLength(b.length)
-			  response.getOutputStream().write(b)
-			}
-			catch (Throwable e) {
-			  println "there was a problem with PDF generation ${e}"
-			  //if(params.template) render(template:params.template)
-			  if(params.pdfController) redirect(controller:params.pdfController, action:params.pdfAction, params:params)
-			  else redirect(uri:params.url + '?' + request.getQueryString())
-			}
-		  }
-	*/
+	
 		  def pdfForm = {
 			try{
 			  byte[] b
@@ -85,14 +47,11 @@ class CouponController {
 			  // def baseUri = g.createLink(uri:"/", absolute:"true").toString()
 			  if(request.method == "GET") {
 				def url = baseUri + params.url + '?' + request.getQueryString()
-				//println "BaseUri is $baseUri"
-				//println "Fetching url $url"
 				b = pdfService.buildPdf(url)
 			  }
 			  if(request.method == "POST"){
 				def content
 				if(params.template){
-				  //println "Template: $params.template"
 				  content = g.render(template:params.template, model:[pdf:params])
 				}
 				else{
@@ -106,7 +65,6 @@ class CouponController {
 			  response.getOutputStream().write(b)
 			}
 			catch (Throwable e) {
-			  println "there was a problem with PDF generation ${e}"
 			  if(params.template) render(template:params.template)
 			  if(params.url) redirect(uri:params.url + '?' + request.getQueryString())
 			  else redirect(controller:params.pdfController, action:params.pdfAction, params:params)
@@ -116,7 +74,6 @@ class CouponController {
 	
 	
 		  def generateCoupon = {
-			  println "${params.pdfId}"
 			  //TODO get the coupon data from service call
 			  
 			  def post = Post.get(params.pdfId);
@@ -125,7 +82,6 @@ class CouponController {
 			  def couponData = new CouponData();
 			  couponData.companyName = post.author.business.name
 			  couponData.companyAddrLine1=post.author.business.address
-//			  couponData.companyAddrLine2= "#232"
 			  couponData.companyCity=post.author.business.city
 			  couponData.companyState=post.author.business.state
 			  couponData.companyPin=post.author.business.areaCode
@@ -142,7 +98,6 @@ class CouponController {
 				  couponData.couponExpirationDate = post.expiresOn.format("MM/dd/yyyy")
 			  }
 			  
-			  //couponData.dealDetails = post.content
 			  couponData.dealDetails = post.title
 			  return ['data':couponData]
 			
